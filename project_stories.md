a5a8c0f82e9cb8b67d69373af2c15e825e77d2fa
t-ateya <ateyaterence@gmail.com>
2024-10-22 15:32:49 -0500

    Fetching platforms

---
+++ project_stories.md
@@ -0,0 +1,7 @@
+Task 1:
+Use React Query to fetch Genre data from the backend and cache it. i.e modify useGenre hook to use react in fetching data from backend.
+
+Solution
+
+- Set up react query: npm i @tanstack/react-query@4.28 or later
+- Install react query dev tools: npm i @tanstack/react-query-devtools@4.28 or later
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+
 
 export interface Genre{
     id: number;
@@ -8,5 +12,15 @@ export interface Genre{
 
 
 //const useGenres = ()=>useData<Genre>('/genres')
-const useGenres = ()=>({data: genres, isLoading: false, error: null})
+//const useGenres = ()=>({data: genres, isLoading: false, error: null})
+
+// Task 1: Use react query to fetch genre from the backend
+const useGenres = () => useQuery({
+    queryKey: ["genres"],
+    queryFn: () => apiClient.get<FetchResponse<Genre>>("/genres").then(res=>res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours since the data hardly changes. 24 hrs * 60 mins * 60 seconds * 1000 ms
+    initialData: {count: genres.length, results: genres},
+
+
+})
 export default useGenres
